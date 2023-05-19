90df2636f98f399bdcaf4b54d1651b84b03ad2c0
ppalladio <nine.slide@gmail.com>
2023-05-19 17:32:04 +0200

    main functionality completed

--- venv/main.py
+++ venv/main.py
@@ -41,3 +41,21 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     print(f'User({update.message.chat.id}) in {message_type}: "{text}"')
     
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text:str=text.replace(BOT_USERNAME, '').strip()
+            response:str=handel_response(new_text)
+        else:
+            return
+    else:
+        response:str=handel_response(text)
+        
+        
+    print('Bot:',response)
+    await update.message.reply_text(response)
+    
+    
+    
+    
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context,error}')
